8c1fdd388c43098d0f847a0fe2bc2d688e331407
adith345 <dexthost650@gmail.com>
2021-09-22 10:39:33 +0530

    Added Bookmark Script

---
+++ book.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+BROWSER="firefox"
+
+declare -a options=( 
+"youtube - https://www.youtube.com"
+"website - https://www.adithpr.herokuapp.com"
+"github_adith - https://www.github.com/adith345"
+"dt_gitlab - https://www.gitlab.com/dwt1"
+"quit"
+)
+choice=$(printf '%s\n' "${options[@]}" | dmenu -h 24 -l 20 -p 'Bookmarks')
+
+if [[ "$choice" == "quit" ]]; then
+    echo "Terminated" && exit 1
+elif [ "$choice" ]; then
+     cfg=$(printf '%s\n' "${choice}" | awk '{printf $NF}')
+     $BROWSER "$cfg"
+else
+    echo "Terminated" && exit 1
+fi
