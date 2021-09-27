14ba81d138aeca65ac2a0f4b0bc7700fe4d91a74
adith345 <dexthost650@gmail.com>
2021-09-27 21:50:19 +0530

    Added Macho

--- emenu.sh
+++ emenu.sh
@@ -10,6 +10,8 @@ declare -a options=(
 "webmenu - $HOME/scripts/webmenu.sh"
 "emenu - $HOME/scripts/emenu.sh"
 "passmenu - $HOME/scripts/passmenu.sh"
+"bspwm - $HOME/.config/bspwm/bspwmrc"
+"sxhkd - $HOME/.config/sxhkd/sxhkdrc"
 "quit"
 )
 choice=$(printf '%s\n' "${options[@]}" | dmenu -h 24 -l 20 -p 'Emenu')
